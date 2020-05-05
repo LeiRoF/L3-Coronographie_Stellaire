8f380b0c0b2d446b2a75866e273bd3e837d326d7
Leirof <vince.lrf@gmail.com>
2020-05-05 15:33:00 +0200

    .

--- simulations.m
+++ simulations.m
@@ -1,8 +1,8 @@
 function simulations
   
-  div = 4;
-  N = 2048/div; % Taille Matrice
-  D = 512/(4*div); % Diamètre pupille
+  lambda = 4;
+  N = 10; % Taille Matrice
+  D = N/lambda; % Diamètre pupille
   Op   = [0.00 0.00 0.00 0.00 0.00]; % Obstruction centrale pupille
   Ol   = [0.00 0.00 0.00 0.00 0.00]; % Obstruction centrale Lyot
   l    = [1.00 0.95 0.90 0.85 0.80]; % Diamètre Lyot (pourcentage pupille)
@@ -16,12 +16,12 @@ function simulations
   i=1;
   name = sprintf('N=%d, D=%d, l=%.2f, Op=%.2f, Ol=%.2f, Without Mask', N, D, l(i), Op(i), Ol(i));
   fprintf('Simulation for: %s ...\n', name);
-  main(div, N, D, Op(i), Ol(i), yc, xc, m, res, l(i), name, i, 0);
+  main(N, D, Op(i), Ol(i), yc, xc, m, res, l(i), name, i, 0);
   
   for i = 1:n
     name = sprintf('N=%d, D=%d, l=%.2f, Op=%.2f, Ol=%.2f, With Mask', N, D, l(i), Op(i), Ol(i));
     fprintf('Simulation for: %s ...\n', name);
-    main(div, N, D, Op(i), Ol(i), yc, xc, m, res, l(i), name, i, 1);
+    main(N, D, Op(i), Ol(i), yc, xc, m, res, l(i), name, i, 1);
   end
   legend
   
